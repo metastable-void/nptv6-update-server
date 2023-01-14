4819327792e04d3b6bcb679c88f8049d035a204b
metastable-void <git@yuka.h09.eu>
2023-01-15 01:37:14 +0900

    refactored

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::str::FromStr;
 use tokio::fs;
 use clap::Parser;
 use hyper::server::conn::AddrStream;
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Request, Response, Server, Method, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
 use tokio::process::Command;
 use url::Url;
@@ -47,7 +47,43 @@ struct Args {
   verbose: Verbosity<InfoLevel>,
 }
 
-fn create_json_responce(status: u16, json: Value) -> Response<Body> {
+struct RequestWithAddr {
+  req: Request<Body>,
+  addr: SocketAddr,
+}
+
+#[derive(Clone)]
+struct Ip6tablesPaths {
+  ip6tables_template: PathBuf,
+  ip6tables_output: PathBuf,
+}
+
+#[derive(Clone)]
+struct Configuration {
+  secret: String,
+  local_prefix: String,
+  ip6tables_paths: Ip6tablesPaths,
+}
+
+impl Configuration {
+  fn secret(&self) -> &str {
+    &self.secret
+  }
+}
+
+#[derive(Clone)]
+struct ServiceError {
+  status: StatusCode,
+  message: String,
+}
+
+impl ServiceError {
+  fn new(status: StatusCode, message: String) -> Self {
+    Self { status, message }
+  }
+}
+
+fn create_json_responce(status: StatusCode, json: Value) -> Response<Body> {
   Response::builder()
     .status(status)
     .header("Content-Type", "application/json")
@@ -55,15 +91,15 @@ fn create_json_responce(status: u16, json: Value) -> Response<Body> {
     .unwrap()
 }
 
-async fn update_ip6tables(req: Request<Body>, _addr: SocketAddr, secret: String, local_prefix: String, ip6tables_template: PathBuf, ip6tables_output: PathBuf) -> Result<Response<Body>, Infallible> {
+async fn update_ip6tables(req_addr: RequestWithAddr, config: Configuration) -> Result<(), ServiceError> {
+  let req = req_addr.req;
+  let Configuration {secret, local_prefix, ip6tables_paths} = config;
+  let Ip6tablesPaths {ip6tables_template, ip6tables_output} = ip6tables_paths;
   let uri = req.uri();
   let path = uri.path();
   let target_path = format!("/{}", secret);
   if path != target_path {
-    log::error!("invalid path: {}", path);
-    return Ok(create_json_responce(404, json!({
-      "error": "not found",
-    })));
+    return Err(ServiceError::new(StatusCode::NOT_FOUND, format!("invalid path: {}", path)))
   }
 
   let url_obj = Url::parse(&req.uri().to_string()).unwrap();
@@ -76,10 +112,7 @@ async fn update_ip6tables(req: Request<Body>, _addr: SocketAddr, secret: String,
     prefix = value.to_string();
   }
   if prefix == "" {
-    log::error!("missing prefix");
-    return Ok(create_json_responce(500, json!({
-      "error": "missing prefix",
-    })));
+    return Err(ServiceError::new(StatusCode::BAD_REQUEST, format!("missing prefix")))
   }
 
   log::debug!("updating ip6tables rules using global prefix {}", &prefix);
@@ -88,10 +121,7 @@ async fn update_ip6tables(req: Request<Body>, _addr: SocketAddr, secret: String,
   if let Ok(content) = fs::read_to_string(&ip6tables_template).await {
     ip6tables_template_content = content;
   } else {
-    log::error!("failed to read ip6tables template: {:?}", &ip6tables_template);
-    return Ok(create_json_responce(500, json!({
-      "error": "failed to read ip6tables template",
-    })));
+    return Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, format!("failed to read ip6tables template: {:?}", &ip6tables_template)))
   }
 
   let ip6tables = ip6tables_template_content
@@ -99,10 +129,7 @@ async fn update_ip6tables(req: Request<Body>, _addr: SocketAddr, secret: String,
     .replace("%GLOBAL_PREFIX%", &prefix);
   
   if let Err(err) = fs::write(&ip6tables_output, ip6tables).await {
-    log::error!("failed to write ip6tables rules: {:?} ({})", &ip6tables_output, err);
-    return Ok(create_json_responce(500, json!({
-      "error": "failed to write ip6tables rules",
-    })));
+    return Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, format!("failed to write ip6tables rules: {:?} ({})", &ip6tables_output, err)))
   }
 
   let ip6tables_file;
@@ -111,10 +138,7 @@ async fn update_ip6tables(req: Request<Body>, _addr: SocketAddr, secret: String,
       ip6tables_file = file.into_std().await;
     },
     Err(err) => {
-      log::error!("failed to open ip6tables rules: {:?} ({})", &ip6tables_output, err);
-      return Ok(create_json_responce(500, json!({
-        "error": "failed to open ip6tables rules",
-      })));
+      return Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, format!("failed to open ip6tables rules: {:?} ({})", &ip6tables_output, err)))
     }
   }
   match Command::new("ip6tables-restore")
@@ -124,24 +148,63 @@ async fn update_ip6tables(req: Request<Body>, _addr: SocketAddr, secret: String,
       Ok(output) => {
         if !output.status.success() {
           let ip6tables_output = String::from_utf8(output.stdout).unwrap_or("".to_string());
-          log::error!("ip6tables-restore failed with exit code {} ({:?})", output.status, &ip6tables_output);
-          return Ok(create_json_responce(500, json!({
-            "error": "ip6tables-restore failed",
-          })))
+          return Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, format!("ip6tables-restore failed with exit code {} ({:?})", output.status, &ip6tables_output)))
         }
       },
       Err(err) => {
-        log::error!("ip6tables-restore failed with {:?}", err);
-        return Ok(create_json_responce(500, json!({
-          "error": "ip6tables-restore failed",
-        })))
+        return Err(ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, format!("ip6tables-restore failed with {:?}", err)))
       }
     }
   
   log::info!("updated ip6tables rules using global prefix: {}", &prefix);
-  Ok(create_json_responce(200, json!({
-    "error": Value::Null,
-  })))
+  Ok(())
+}
+
+/// Handle incoming HTTP requests
+/// For security, this does not output on success
+async fn handle_request(req_addr: RequestWithAddr, config: Configuration) -> Result<(), ServiceError> {
+  let req = req_addr.req;
+  let addr = req_addr.addr;
+  let method = req.method();
+  let uri = req.uri();
+  let secret = config.secret();
+
+  log::debug!("{}: {} {}", &addr, method, uri);
+
+  let target_path = format!("/update/{}", secret);
+  match (method, uri.path().to_string() == target_path) {
+    (&Method::GET, true) => {
+      let req_addr = RequestWithAddr {req, addr: addr};
+      update_ip6tables(req_addr, config).await
+    },
+
+    (_, true) => {
+      Err(ServiceError::new(StatusCode::METHOD_NOT_ALLOWED, format!("invalid method: {}", method)))
+    },
+
+    (&Method::GET, false) => {
+      Err(ServiceError::new(StatusCode::NOT_FOUND, format!("invalid path: {}", uri)))
+    },
+
+    _ => {
+      Err(ServiceError::new(StatusCode::BAD_REQUEST, format!("invalid request: {}", req.uri())))
+    }
+  }
+}
+
+async fn format_json_responce(req_addr: RequestWithAddr, config: Configuration) -> Result<Response<Body>, Infallible> {
+  match handle_request(req_addr, config).await {
+    Ok(_) => Ok(create_json_responce(StatusCode::OK, json!({
+      "error": Value::Null,
+    }))),
+
+    Err(err) => {
+      log::error!("error: {}", &err.message);
+      Ok(create_json_responce(err.status, json!({
+        "error": &err.message,
+      })))
+    }
+  }
 }
 
 #[tokio::main]
@@ -164,14 +227,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     async move {
       let addr = addr.clone();
       Ok::<_, Infallible>(service_fn(move |req : Request<Body>| {
-          update_ip6tables(req, addr, secret.clone(), local_prefix.clone(), ip6tables_template.clone(), ip6tables_output.clone())
+        let req_addr = RequestWithAddr {
+          req: req,
+          addr: addr.clone(),
+        };
+        let ip6tables_paths = Ip6tablesPaths {
+          ip6tables_template: ip6tables_template.clone(),
+          ip6tables_output: ip6tables_output.clone(),
+        };
+        let config = Configuration {
+          secret: secret.clone(),
+          local_prefix: local_prefix.clone(),
+          ip6tables_paths: ip6tables_paths,
+        };
+        format_json_responce(req_addr, config)
       }))
       }
   });
 
   let server = Server::bind(&addr).serve(make_svc);
 
-  log::info!("Listening on http://{}", &addr);
+  log::info!("Listening on http://{} - awaiting requests at /update/<secret>", &addr);
   // Run this server for... forever!
   if let Err(e) = server.await {
     log::error!("server error: {}", e);
